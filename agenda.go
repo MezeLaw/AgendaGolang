0452c567188cb76106edfb1b2793f2d87bb433a2
angelfcerchi <Sorete2340>
2022-11-22 21:37:56 -0300

    primer commit de la funciones del hanlder y func aux

--- agenda.go
+++ agenda.go
@@ -1,6 +1,11 @@
 package main
 
-import "agenda/modelos"
+import (
+	"agenda/modelos"
+	"errors"
+	"strconv"
+	"strings"
+)
 
 type ServicioDeLaAgenda interface {
 	/*
@@ -34,10 +39,29 @@ func NewAgenda(servicio ServicioDeLaAgenda) Agenda {
 	Pre: recibe el contacto a agendar
 	Post: retorna el id del contacto agendado y/o un error segun corresponda
 */
-
 func (a *Agenda) Agendar(contacto modelos.Contacto) (*string, error) {
-
-	return nil, nil
+	if contacto.DNI == "" {
+		return nil, errors.New("dni perdido")
+	}
+	dni, err := validadorNumerosStr(contacto.DNI)
+	if err != nil {
+		return &dni, err
+	}
+	if contacto.Nombre == "" {
+		return nil, errors.New("nombre perdido")
+	}
+	if contacto.Telefono == "" {
+		return nil, errors.New("telefono perdido")
+	}
+	telefono, err := validadorNumerosStr(contacto.Telefono)
+	if err != nil {
+		return &telefono, err
+	}
+	c, err := a.servicio.AgregarContacto(contacto)
+	if err != nil {
+		return &c.DNI, err
+	}
+	return &c.DNI, nil
 }
 
 /*
@@ -45,7 +69,18 @@ func (a *Agenda) Agendar(contacto modelos.Contacto) (*string, error) {
 	Post: retorna el contacto consultado y/o un error
 */
 func (a *Agenda) VerContacto(dni string) (*modelos.Contacto, error) {
-	return nil, nil
+	if dni == "" {
+		return nil, errors.New("DNI perdido")
+	}
+	dni, err := validadorNumerosStr(dni)
+	if err != nil {
+		return nil, err
+	}
+	c, err := a.servicio.ConsultarContato(dni)
+	if err != nil {
+		return nil, err
+	}
+	return c, nil
 }
 
 /*
@@ -53,5 +88,28 @@ func (a *Agenda) VerContacto(dni string) (*modelos.Contacto, error) {
 	Post: Retorna el doc del contacto eliminado y/o un error segun corresponda
 */
 func (a *Agenda) BorrarContacto(dni string) (*string, error) {
-	return nil, nil
+	if dni == "" {
+		strings := "DNI Invalido"
+		return &strings, errors.New("dni perdido")
+	}
+	dni, err := validadorNumerosStr(dni)
+	if err != nil {
+		return &dni, err
+	}
+	dniEliminado, err := a.servicio.EliminarContacto(dni)
+	if err != nil {
+		return nil, err
+	}
+	return dniEliminado, nil
+}
+
+func validadorNumerosStr(numero string) (string, error) {
+	if strings.Contains(numero, ".") || strings.Contains(numero, ",") {
+		return numero, errors.New("No debe contener ',' o '.' ")
+	}
+	_, err := strconv.Atoi(numero)
+	if err != nil {
+		return numero, errors.New("DNI ingresado con caracter invalido")
+	}
+	return numero, nil
 }
